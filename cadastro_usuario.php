bdab96b438fab139a37bdce20cd05ed6f5c1ce37
Kauan20O7 <Kauan_fernandes3@estudante.sesisenai.org.br>
2025-10-31 12:53:25 -0300

    update

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -8,9 +8,6 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Cadastro de novos usuários no Sistema de Gerenciamento de Tarefas">
     <title>Cadastro de Usuário</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
